b15bd4f9f0245ea350fdb4ab46fb091ec89e0d7b
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2018-06-18 13:45:27 +0200

    Start repo

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '869408879731-gqc1teg4hu0cj780tj8vttv8nof4m6is.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '1wKFlu1xXc6FHgtpQlI7d7Ww',
+	'CALLBACK_URL':'http://localhost:3000/auth/google/callback'
+};
+
+/*
+869408879731-gqc1teg4hu0cj780tj8vttv8nof4m6is.apps.googleusercontent.com
+1wKFlu1xXc6FHgtpQlI7d7Ww
+*/
